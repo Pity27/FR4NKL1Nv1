3e262d32c23492c5cef6c3ba92d54901dd32fcbb
Pity27 <48308798+Pity27@users.noreply.github.com>
2019-03-07 14:06:11 +0800

    Create v1.sh

---
+++ v1.sh
@@ -0,0 +1,209 @@
+#MauNgapainThod
+#RecodeGakBerFaedahTwLah
+#hussSana
+#cumanToolsSederhana
+
+clear
+black='\e[0;30m'
+blue='\e[0;34m'
+green='\e[0;32m'
+cyan='\e[0;36m'
+red='\e[0;31m'
+purple='\e[0;35m'
+brown='\e[0;33m'
+lightgray='\e[0;37m'
+darkgray='\e[1;30m'
+lightblue='\e[1;34m'
+lightgreen='\e[1;32m'
+lightcyan='\e[1;36m'
+lightred='\e[1;31m'
+lightpurple='\e[1;35m'
+yellow='\e[1;33m'
+white='\e[1;37m'
+clear
+
+echo""
+echo -e $lightcyan "                        O O                    "
+echo -e $lightcyan "                       dO Ob                   "
+echo -e $lightcyan "                      dOO OOO                  "
+echo -e $lightcyan "                     dOOO OOOb                 "
+echo -e $lightcyan "                    dOOOO OOOOb                "
+echo -e $lightcyan "                    OOOOO OOOOO                "
+echo -e $lightcyan "                    OOOOO OOOOO                "
+echo -e $lightcyan "                    OOOOO OOOOO                "
+echo -e $lightcyan "                    YOOOO OOOOO                "
+echo -e $lightcyan "                     YOOO OOOP                 "
+echo -e $lightcyan "                oOOOOOOOOOOOOb                 "
+echo -e $lightcyan "              oOOOOOOOOOOOOOOOb                "
+echo -e $lightcyan "             oOOOb dOOOOOOOOOOO                "
+echo -e $lightcyan "            OOOOOOOOOOOOOOOOOOO                "
+echo -e $lightcyan "            OOOOOOOOOOOOOOOOOOP                "
+echo -e $lightcyan "            OOOOOOOOOOOOOOOOOP                 "
+echo -e $lightcyan "             YOOOOOOOOOOOOOOP                  "
+echo -e $lightcyan "               YOOOOOOOOOOOP                   "
+echo -e $lightcyan "              %%%%%%%%%%%%%%                   "
+echo -e $lightcyan "             %%%%%%OOOjgsOOO                   "
+echo -e $purple "          ==========================           "
+echo -e $yellow "          # AuthorBy : Mr.FR4NKL1N #           "
+echo -e $purple "          # WA : 081274975359      #           "
+echo -e $blue "          # Team : Gak Punya Team  #     "
+echo -e $yellow "          # Gunakan Tools Ini dgn  #           "
+echo -e $cyan "            #          BIJAK         #    "
+echo -e $purple "          ==========================           "
+echo""
+echo -e $lightred "Silahkan" $white "Pilih Toolsnya"
+echo""
+echo -e $white "[===============================================]>"
+echo -e $lightcyan "1.> Script Deface Creator"
+echo -e $blue "[===============================================]>"
+echo -e $lightred "2.> weeman Buat Phising"
+echo -e $yellow "[===============================================]>"
+echo -e $lightgreen "3.> Auto Report Facebook"
+echo -e $purple "[===============================================]>"
+echo -e $lightred "4.> MBF"
+echo -e $white "[===============================================]>"
+echo -e $lightpurple "5.> Install PhisingGame Buat Hack ML & COC"
+echo -e $blue "[===============================================]>"
+echo -e $lightgray "6.> DDOS Website Dengan Hammer"
+echo -e $green "[===============================================]>"
+echo -e $darkgray "7.> Install RED_HAWK"
+echo -e $purple "[===============================================]>"
+echo -e $blue "8.> Install InstaHack Buat Hack Instagram"
+echo -e $lightred "[===============================================]>"
+echo -e $white "9.> Install viSQL "
+echo -e $yellow "[===============================================]>"
+echo -e $lightcyan "10.> Install Mr.B4J1N64N v6"
+echo -e $lightpurple "[===============================================]>"
+echo -e $white "99.> Exit"
+echo -e $blue "[===============================================]>"
+echo
+echo -e $lightcyan
+read -p "╭─[Pilih Nomer]
+╰─FR4NKL1N=•>" p
+
+if [ $p = 1 ] || [ $p = 1 ]
+then
+clear
+echo -e $yellow "Installing Script Deface Creator"
+apt update && apt upgrade -y
+apt install python2
+git clone https://github.com/Ubaii/script-deface-creator
+echo -e $lightred "T E R I N S T A L L"
+fi
+
+if [ $p = 2 ] || [ $p = 2 ]
+then
+clear
+echo -e $yellow "Installing Weeman"
+apt update && apt upgrade -y
+apt install git
+apt install php
+git clone https://github.com/evait-security/weeman.git
+echo -e $lightred "T E R I N S T A L L"
+fi
+
+if [ $p = 3 ] || [ $p = 3 ]
+then
+clear
+echo -e $yellow "Installing Auto Report Facebook"
+apt update && apt upgrade -y
+apt install git
+apt install ruby
+git clone https://github.com/gshofficialgithubindonesia/autoreport-fb
+echo -e $lightred "T E R I N S T A L L"
+fi
+
+if [ $p = 4 ] || [ $p = 4 ]
+then
+clear
+echo -e $yellow "Installing MBF"
+apt update && apt upgrade -y
+apt install python2
+pip2 install mechanize
+git clone https://github.com/tikuskecil/multi-bruteforce-facebook
+echo -e $lightred "T E R I N S T A L L"
+fi
+
+if [ $p = 5 ] || [ $p = 5 ]
+then
+clear
+echo -e $yellow "Installing PhisingGame Buat Hack ML & COC"
+apt update && apt upgrade -y
+pkg install python2
+git clone https://github.com/senitopeng/PhisingGame.git
+echo -e $lightred "T E R I N S T A L L"
+fi
+
+if [ $p = 6 ] || [ $p = 6 ]
+then
+clear
+echo -e $yellow "Installing DDOS Hammer"
+apt update && apt upgrade -y
+pkg install python
+git clone http://github.com/cyweb/hammer
+echo -e $lightred "T E R I N S T A L L"
+fi
+
+if [ $p = 7 ] || [ $p = 7 ]
+then
+clear
+echo -e $yellow "Installing RED_HAWK"
+apt update && apt upgrade -y
+apt install git
+apt install php
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+echo -e $lightred "T E R I N S T A L L"
+fi
+
+if [ $p = 8 ] || [ $p = 8 ]
+then
+clear
+echo -e $yellow "Installing InstaHack"
+apt update && apt upgrade -y
+apt install python2
+apt install git
+git clone https://github.com/avramit/Instahack
+echo -e $lightred "T E R I N S T A L L"
+fi
+
+if [ $p = 9 ] || [ $p = 9 ]
+then
+clear
+echo -e $yellow "Installing viSQL"
+apt update && apt upgrade -y
+apt install python2
+apt install git
+git clone https://github.com/blackvkng/viSQL
+echo -e $lightred "T E R I N S T A L L"
+fi
+
+if [ $p = 10 ] || [ $p = 10 ]
+then
+clear
+echo -e $yellow "Installing Mr.B4J1N64N v6"
+apt update && apt upgrade -y
+pip2 install termcolor
+apt install toilet
+apt install ruby
+gem install lolcat
+apt install git
+git clone https://github.com/DarknessCyberTeam/BAJINGANv6
+echo -e $lightred "T E R I N S T A L L"
+fi
+
+if [ $p = 99 ] || [ $p = 99 ]
+then
+echo -e $lightcyan "Thank's for using my tools"
+sleep 1
+echo -e $lightgreen "I Am The Mr.FR4NKL1N"
+sleep 1
+echo -e $lightred "HACKING is Not CRIMINAL"
+sleep 1
+echo -e $lightpurple "SARAN KALIAN BISA SAYA TERIMA DI WA SAYA"
+sleep 1
+echo -e $lightblue "WA : 081274975359"
+sleep 1
+echo -e $white "Terima Kasih Semua"
+sleep 1
+exit
+fi
